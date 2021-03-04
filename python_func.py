61600de72503416f0a2f98f0fc361e32ed6bfdc6
Weikai.Kong <13667195893@163.com>
2021-03-04 11:27:18 +0800

    upload new py file.

---
+++ python_func.py
@@ -0,0 +1,17 @@
+
+# 1) 使用import将库函数导入进来
+'''
+import random #库
+for i in range(5):
+    print(random.randint(1, 10))  #[1~10]  randint是random库的函数
+'''
+'''
+#  2) from lib import fnc 用法
+from random import randint #库及函数名
+for i in range(5):
+    print(randint(1, 10))  #[1~10]  randint是random库的函数
+'''
+#  3) from lib import* 用法
+from random import * #库及 所有 函数
+for i in range(5):
+    print(randint(1, 10))  #[1~10]  randint是random库的函数
